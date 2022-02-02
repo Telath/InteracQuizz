ab33e5a050da57e87841ea238bad8ec7b66595a3
Yohann <yohann.moy@gmail.com>
2022-02-02 15:47:14 +0100

    Settings done

--- webfiles/config/Config.php
+++ webfiles/config/Config.php
@@ -1,4 +1,26 @@
 <?php 
 
-    echo $_SERVER['DOCUMENT_ROOT'];
+    class Config{
+        private $settingsArray;
+        private static $instance;
+
+        private function __construct(){
+            $this->settingsArray = json_decode(file_get_contents(__DIR__."\settings.json"), true);
+            return $this->settingsArray;
+        }
+        
+        public static function allSettings():array{
+            if(self::$instance == NULL){
+                self::$instance = new Config();
+            }
+
+            return self::$instance->settingsArray;
+        }
+        
+        public static function databaseSettings():array{
+            return self::allSettings()["database"];
+        } 
+    }
+    
+
 
