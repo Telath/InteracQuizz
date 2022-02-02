ea6ced5ebe29305f1f366ea2b76f595534693b9f
Jonathan NOIRET <88317172+JonathanNOIRET@users.noreply.github.com>
2022-02-02 15:41:46 +0100

    connexion bdd

---
+++ webfiles/architecture/models/Notes.php
@@ -0,0 +1,18 @@
+<?php
+
+    class Notes{
+
+        private $id;
+
+        public static function addNote(){
+
+        }
+
+        public static function updateNote(){
+
+        }
+
+        public static function deleteNote($id){
+            $req = "SELECT FROM notes WHERE id=$id"
+        }
+    }
