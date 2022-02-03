142079a5966ffd898ffb88bee8f2297d4c362e03
Jonathan NOIRET <88317172+JonathanNOIRET@users.noreply.github.com>
2022-02-03 15:31:33 +0100

    footer/header and homepage

--- webfiles/app/views/home.php
+++ webfiles/app/views/home.php
@@ -1,3 +1,29 @@
 <?php 
     echo "Homepage view";
+    require_once "C:\laragon\www\InteracQuizz\webfiles\app\controllers\UserController.php";
+    
 ?>
+
+<?php include_once 'header.php'; ?>
+
+    <body>
+        <h2>Connectez-vous</h2>
+            <form action="<?php UserController::userConnexion();?>" method="POST">
+                <div><input type="text" name="mail" placeholder="Mail"></div>
+                <div><input type="password" name="motdepasse" placeholder="Mot de passe"></div>
+                <div><input type="submit" value="Connexion"></div>
+            </form>
+        
+        <h2>Inscrivez-vous</h2>
+            <form action="<?php UserController::userRegister();?>" method="POST">
+                <div><input type="text" name="nom" placeholder="Votre nom :"></div>
+                <div><input type="text" name="prenom" placeholder="Votre prénom :"></div>
+                <div><input type="mail" name="mail" placeholder="Votre mail :"></div>
+                <div><input type="password" name="motdepasse" placeholder="Votre Mot de passe :"></div>
+                <div><input type="submit" value="Inscription"></div>
+            </form>
+
+
+<?php include_once 'footer.php'; ?>
+</body>
+</html>
